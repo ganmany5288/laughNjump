9939dcfe8dd3ee54bf2ae5aa7c5a2e5940655dc0
James Tang <52274101+ganmany5288@users.noreply.github.com>
2024-01-27 14:02:30 -0800

    yesy

--- PissBiz/Options/Video/video.gd
+++ PissBiz/Options/Video/video.gd
@@ -1,18 +1,5 @@
 extends Control
 
-func _ready():
-
-	var screen_type = Persistent.config.get_value("Video", "fullscreen")
-	
-	if screen_type == DisplayServer.WINDOW_MODE_FULLSCREEN:
-		%FullScreen.button_pressed = true
-		
-		
-	var borderless = Persistent.config.get_value("Video", "borderless")
-	
-	if borderless == DisplayServer.WINDOW_MODE_FULLSCREEN:
-		%FullScreen.button_pressed = true
-	
 
 
 
@@ -20,11 +7,9 @@ func _ready():
 func _on_full_screen_toggled(toggled_on):
 	if toggled_on:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-		Persistent.config.set_value("Video", "fullscreen", DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-		Persistent.config.set_value("Video", "fullscreen", DisplayServer.WINDOW_MODE_WINDOWED)
-	Persistent.save_data()
+
 
 
 
