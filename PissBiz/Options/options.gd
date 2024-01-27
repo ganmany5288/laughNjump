0b52d4ed847d3630ad0388c155bdb2b34578980e
James Tang <52274101+ganmany5288@users.noreply.github.com>
2024-01-27 14:28:38 -0800

    Working Start/Option UI Part 1

--- PissBiz/Options/options.gd
+++ PissBiz/Options/options.gd
@@ -4,12 +4,15 @@ extends Control
 
 
 func _on_volume_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Options/Audio/audio.tscn")
 
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://Menu/menu.tscn")
 
 
+
+
+
 func _on_video_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Options/Video/video.tscn")
