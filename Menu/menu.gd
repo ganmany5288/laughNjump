3603e159a98992638e0737ea61d5e8504a8cfc2e
James Tang <rhsjamestang2019@gmail.com>
2024-01-27 20:00:18 -0800

    Menu For real update

--- Menu/menu.gd
+++ Menu/menu.gd
@@ -1,16 +1,12 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func _on_Play_pressed():
+	get_tree().change_scene_to_file("res://Levels/LevelTest/leveltest.tscn")
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _on_Options_pressed():
+	get_tree().change_scene_to_file("res://Options/options.tscn")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Quit_pressed():
+	get_tree().quit()
--- Options/options.gd
+++ Options/options.gd
@@ -3,16 +3,20 @@ extends Control
 
 
 
-func _on_volume_pressed():
+
+func _on_audio_pressed():
 	get_tree().change_scene_to_file("res://Options/Audio/audio.tscn")
-
-
-func _on_back_pressed():
-	get_tree().change_scene_to_file("res://Menu/menu.tscn")
-
-
+	
+	
 
 
 
 func _on_video_pressed():
 	get_tree().change_scene_to_file("res://Options/Video/video.tscn")
+	
+	
+	
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://Menu/menu.tscn")
