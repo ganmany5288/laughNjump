a13b1590bd02d5c549a97647e2a425bd2bb3d8d5
James Tang <rhsjamestang2019@gmail.com>
2024-01-28 10:18:53 -0800

    asdf

---
+++ test_scene/Audio.gd
@@ -0,0 +1,15 @@
+extends TabBar
+
+
+
+
+func _on_master_value_changed(value):
+	pass # Replace with function body.
+
+
+func _on_music_value_changed(value):
+	pass # Replace with function body.
+
+
+func _on_sfx_value_changed(value):
+	pass # Replace with function body.
